c5807c5611bcdd4210e4a04b9d2e4ade836f4215
anastasiarblv <rublevaae@gmail.com>
2024-12-24 03:11:07 +0300

    Version_3

--- docker/kafka-consumer/python-consumer.py
+++ docker/kafka-consumer/python-consumer.py
@@ -1,20 +1,17 @@
+from os import environ ## для чтения переменных среды, для подключения к базе данных
 from kafka import KafkaConsumer
 import json
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import psycopg2
+from sqlalchemy import create_engine, text
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 
 nltk.download('vader_lexicon')
 analyzer = SentimentIntensityAnalyzer()
 
-
-POSTGRES_DB='postgres'
-POSTGRES_USER= 'postgres'
-POSTGRES_PASSWORD='postgres'
-container_name_postgres ='postgres'
-PORT = '5432'
-conn = psycopg2.connect(dbname=POSTGRES_DB, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host=container_name_postgres, port = PORT)
-cur = conn.cursor()
+DATABASE_URL = environ.get('DATABASE_URL')
+engine = create_engine(DATABASE_URL)
 
 kafka_nodes = "kafka:9092"
 myTopic = "sentence"
@@ -22,11 +19,45 @@ myTopic = "sentence"
 consumer = KafkaConsumer(myTopic,
                         bootstrap_servers=kafka_nodes,
                         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
-for message in consumer:
-    data = message.value
-    print(data)
-    scores = analyzer.polarity_scores(data['sentence'])
-    print(scores['compound'])
-    #Вставляем данные в Postgres
-    cur.execute("INSERT INTO sentences (sentence, sentiment) VALUES(%s, %s)", (data['sentence'], scores['compound']))
-    conn.commit()
+with engine.connect() as conn:
+    for message in consumer:
+        data = message.value
+        print(data)
+        #sentence = data['sentence']
+        scores = analyzer.polarity_scores(data['sentence'])
+        #compound = scores['compound']
+        print(scores['compound'])
+        #Вставляем данные в Postgres
+        insert_query_string = f"INSERT INTO sentences (sentence, sentiment) VALUES(:sentence, :sentiment)"
+        #conn.execute(text(insert_query_string),your_parameters)
+        conn.execute(text(insert_query_string), dict(sentence = data['sentence'], sentiment = scores['compound']))
+        #                          parameters = dict(sentence = sentence, sentiment = compound)
+        conn.commit()
+
+#import sqlalchemy
+#from sqlalchemy import create_engine
+#engine = create_engine('mysql://admin:passkey@svc-3nb226d8-ee13-47f0-8ca4-2dc820773442-dml.aws-oregon-2.svc.singlestore.com:3306/dbTest')
+#conn = engine.connect()
+#conn.execute(text("CREATE TABLE stock (ID INT, Code VARCHAR(4), Qty INT)"))
+#conn.execute(text("INSERT INTO stock VALUES (1,'xvfg',23)"))
+#conn.execute(text("INSERT INTO stock VALUES (2,'dtsi',12)"))
+#conn.execute(text("INSERT INTO stock VALUES (3,'rtky',8)"))
+#conn.close()
+
+#with db.connect() as conn:
+#    conn.execute(statement=query, parameters=dict(_age=5, _country='US'))
+
+#with db.connect() as conn:
+#    conn.execute(statement=query, parameters=dict(_age=5, _country='US'))
+
+#connection = st.experimental_connection(name = 'ghost', type = 'sql', autocommit = True)
+#with connection.session as session:
+#    session.execute('INSERT INTO document (ID, SENTENCE) VALUES(:id, :sen);', params = dict(id = id, sen = answer))
+#    session.commit()
+
+# "commit as you go"
+#with engine.connect() as conn:
+#    conn.execute(text("INSERT INTO some_table (x, y) VALUES (:x :y)"),[{"x": 1, "y": 1}, {"x": 2, "y": 4}])
+#    conn.commit()
+
+
